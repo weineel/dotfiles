d4a9e63664352677abea93f29bb496a56eed2187
weineel <LiJF_wn@163.com>
2024-05-20 13:26:39 +0800

    feat: vue 模版文件快速跳转到 template、script 、 style

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,32 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local map = vim.keymap.set
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "vue",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    map(
+      "n",
+      "<leader>vt",
+      ":call search('^<template', 'bw')<CR>",
+      { desc = "Go to Vue template", noremap = true, silent = true }
+    )
+    map(
+      "n",
+      "<leader>vs",
+      ":call search('^<script', 'bw')<CR>",
+      { desc = "Go to Vue script", noremap = true, silent = true }
+    )
+    map(
+      "n",
+      "<leader>vd",
+      ":call search('^<style', 'bw')<CR>",
+      { desc = "Go to Vue style", noremap = true, silent = true }
+    )
+  end,
+})
