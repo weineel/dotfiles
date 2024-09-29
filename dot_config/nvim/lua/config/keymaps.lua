29d446d6439308ae6c44f749f730c3de16ff05fd
weineel <LiJF_wn@163.com>
2024-09-29 16:09:21 +0800

    feat: 更新 neovim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -9,11 +9,23 @@ if vim.g.vscode then
   vim.api.nvim_create_autocmd("User", {
     pattern = "LazyVimKeymaps",
     callback = function()
-      vim.keymap.set("n", "<leader><space>", "<cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup')<CR>")
-      vim.keymap.set("n", "<leader>fb", "<cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditor')<CR>")
+      vim.keymap.set(
+        "n",
+        "<leader><space>",
+        "<cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup')<CR>"
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>fb",
+        "<cmd>call VSCodeNotify('workbench.action.quickOpenPreviousRecentlyUsedEditor')<CR>"
+      )
       vim.keymap.set("n", "<leader>ff", "<cmd>Find<cr>")
       vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
       vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+      vim.keymap.set("n", "gi", [[<cmd>call VSCodeNotify('editor.action.goToImplementation')<cr>]])
+      vim.keymap.set("n", "gI", [[<cmd>call VSCodeNotify('references-view.findImplementations')<cr>]])
+      vim.keymap.set("n", "gr", [[<cmd>call VSCodeNotify('editor.action.goToReferences')<cr>]])
+      vim.keymap.set("n", "gR", [[<cmd>call VSCodeNotify('references-view.findReferences')<cr>]])
     end,
   })
 end
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,16 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<leader><space>", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-    -- { "<leader>fG", "<cmd>Telescope find_files<cr>", desc = "Find Files (all files)" }
+    {
+      "<leader>fG",
+      function()
+        require("telescope.builtin").find_files({
+          no_ignore = true,
+          hidden = true,
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/node_modules/*" },
+        })
+      end,
+      desc = "Find Files (all files, excluding node_modules/.git)",
+    },
   },
 }
